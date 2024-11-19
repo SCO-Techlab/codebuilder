485db9cbd405d2416e3f4cd923e795d2bec2867d
SCO-Techlab <sco.techlab@gmail.com>
2024-11-19 17:33:31 +0100

    rename crons service path

--- sco-codebuilder-backend/src/modules/writter/writter.module.ts
+++ sco-codebuilder-backend/src/modules/writter/writter.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WritterController } from './writter.controller';
 import { WritterService } from './writter.service';
-import { WritterCronsService } from './writter.crons.service.ts.service';
+import { WritterCronsService } from './writter.crons.service';
 
 @Module({
   controllers: [
